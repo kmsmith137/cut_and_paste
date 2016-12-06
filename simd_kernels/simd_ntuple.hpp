ce140b12c2ba9d13c489fdc6c505f83771442ac4
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-05 16:10:06 -0800

    more work on simd wrapper classes

--- simd_kernels/simd_ntuple.hpp
+++ simd_kernels/simd_ntuple.hpp
@@ -10,6 +10,8 @@ struct simd_ntuple
     simd_ntuple<T,S,N-1> v;
     simd_t<T,S> x;
 
+    simd_ntuple() { }
+
     inline void setzero()
     {
 	v.setzero();
@@ -27,6 +29,86 @@ struct simd_ntuple
 	v.storeu(p);
 	x.storeu(p+(N-1)*S);
     }
+
+    inline simd_ntuple<T,S,N> &operator+=(const simd_ntuple<T,S,N> &t)
+    {
+	v += t.v;
+	x += t.x;
+	return *this;
+    }
+
+    inline simd_ntuple<T,S,N> &operator-=(const simd_ntuple<T,S,N> &t)
+    {
+	v -= t.v;
+	x -= t.x;
+	return *this;
+    }
+
+    inline simd_ntuple<T,S,N> &operator*=(const simd_ntuple<T,S,N> &t)
+    {
+	v *= t.v;
+	x *= t.x;
+	return *this;
+    }
+
+    inline simd_ntuple<T,S,N> &operator/=(const simd_ntuple<T,S,N> &t)
+    {
+	v /= t.v;
+	x /= t.x;
+	return *this;
+    }
+
+    inline simd_ntuple<T,S,N> operator+(const simd_ntuple<T,S,N> &t) const
+    {
+	simd_ntuple<T,S,N> ret;
+	ret.v = v + t.v;
+	ret.x = x + t.x;
+	return ret;
+    }
+
+    inline simd_ntuple<T,S,N> operator-(const simd_ntuple<T,S,N> &t) const
+    {
+	simd_ntuple<T,S,N> ret;
+	ret.v = v - t.v;
+	ret.x = x - t.x;
+	return ret;
+    }
+
+    inline simd_ntuple<T,S,N> operator*(const simd_ntuple<T,S,N> &t) const
+    {
+	simd_ntuple<T,S,N> ret;
+	ret.v = v * t.v;
+	ret.x = x * t.x;
+	return ret;
+    }
+
+    inline simd_ntuple<T,S,N> operator/(const simd_ntuple<T,S,N> &t) const
+    {
+	simd_ntuple<T,S,N> ret;
+	ret.v = v / t.v;
+	ret.x = x / t.x;
+	return ret;
+    }
+
+    inline simd_t<T,S> vertical_sum() const  { return v.vertical_sum() + x; }
+
+    inline T sum() const { return this->vertical_sum().sum(); }
+    
+    inline T compare(const T *p) const
+    {
+	const simd_ntuple<T,S,N> &a = *this;
+	
+	simd_ntuple<T,S,N> b;
+	b.loadu(p);
+	
+	simd_ntuple<T,S,N> t = (a-b) * (a-b);
+	float num = t.sum();
+	
+	t = a*a + b*b;
+	float den = t.sum();
+	
+	return (den > 0.0) ? sqrt(num/den) : 0.0;
+    }
 };
 
 
@@ -36,7 +118,18 @@ struct simd_ntuple<T,S,0>
     inline void setzero() { }
     inline void loadu(const T *p) { }
     inline void storeu(T *p) const { }
+
+    inline simd_ntuple<T,S,0> operator+(const simd_ntuple<T,S,0> &t) const { return simd_ntuple<T,S,0>(); }
+    inline simd_ntuple<T,S,0> operator-(const simd_ntuple<T,S,0> &t) const { return simd_ntuple<T,S,0>(); }
+    inline simd_ntuple<T,S,0> operator*(const simd_ntuple<T,S,0> &t) const { return simd_ntuple<T,S,0>(); }
+    inline simd_ntuple<T,S,0> operator/(const simd_ntuple<T,S,0> &t) const { return simd_ntuple<T,S,0>(); }
+
+    inline simd_t<T,S> vertical_sum() const { return 0; }
 };
+
+
+//template<typename T, unsigned int S> 
+//inline simd_t<T,S> simd_ntuple<T,S,1>::vertical_sum() const { return this->x; }
 
 
 #endif // _NTUPLE_HPP
--- simd_kernels/simd_t.hpp
+++ simd_kernels/simd_t.hpp
@@ -22,6 +22,35 @@ template<> struct simd_t<float,4>
 
     inline void store(float *p) const  { _mm_store_ps(p,x); }
     inline void storeu(float *p) const { _mm_storeu_ps(p,x); }
+
+    template<unsigned int M> 
+    inline float extract() const
+    {
+	// _mm_extract_ps() returns int instead of float
+	union { int i; float x; } u;
+	u.i = _mm_extract_ps(x, M);
+	return u.x;
+    }
+
+    // Returns the horizontal sum of elements as a constant simd vector [ x ... x ].
+    inline simd_t<float,4> horizontal_sum() const
+    {
+	__m128 y = x + _mm_shuffle_ps(x, x, 0xb1);   // (2301)_4 = 0xb1
+	return y + _mm_shuffle_ps(y, y, 0x4e);       // (1032)_4 = 0x4e
+    }
+
+    // Returns the horizontal sum of elements as a scalar.
+    inline float sum() const
+    {
+	simd_t<float,4> y = this->horizontal_sum();
+	
+	// For some reason calling extract<0>() here gives a compiler error,
+	// so we cut-and-paste the logic.
+
+	union { int i; float x; } u;
+	u.i = _mm_extract_ps(y.x, 0);
+	return u.x;
+    }
 };
 
 
@@ -32,7 +61,8 @@ template<> struct simd_t<float,8>
     simd_t() { }
     simd_t(__m256 y) { x = y; }
     simd_t(float y)  { x = _mm256_set1_ps(y); }
-    
+
+    inline void set1(float y) { x = _mm256_set1_ps(y); }
     inline void setzero() { x = _mm256_setzero_ps(); }
 
     inline void load(const float *p)  { x = _mm256_load_ps(p); }
@@ -40,8 +70,37 @@ template<> struct simd_t<float,8>
 
     inline void store(float *p) const  { _mm256_store_ps(p,x); }
     inline void storeu(float *p) const { _mm256_storeu_ps(p,x); }
+
+    template<unsigned int M> 
+    inline float extract() const
+    {
+	simd_t<float,4> x2 = _mm256_extractf128_ps(x, M/4);
+	return x2.extract<M%4> ();
+    }
+
+    // Returns the horizontal sum of elements as a constant simd vector [ x ... x ].    
+    inline simd_t<float,8> horizonal_sum() const
+    {
+	__m256 y = x + _mm256_shuffle_ps(x, x, 0xb1);   // (2301)_4 = 0xb1
+	y += _mm256_shuffle_ps(y, y, 0x4e);             // (1032)_4 = 0x4e
+	return y + _mm256_permute2f128_ps(y, y, 0x01);
+    }
+
+    // Returns the horizontal sum of elements as a scalar.
+    inline float sum() const
+    {
+	__m256 y = x + _mm256_permute2f128_ps(x, x, 0x01);
+
+	simd_t<float,4> z = _mm256_extractf128_ps(y, 0);
+	return z.sum();
+    }
 };
 
+
+template<typename T, unsigned int S> inline simd_t<T,S> &operator+=(simd_t<T,S> &a, simd_t<T,S> b) { a.x += b.x; return a; }
+template<typename T, unsigned int S> inline simd_t<T,S> &operator-=(simd_t<T,S> &a, simd_t<T,S> b) { a.x -= b.x; return a; }
+template<typename T, unsigned int S> inline simd_t<T,S> &operator*=(simd_t<T,S> &a, simd_t<T,S> b) { a.x *= b.x; return a; }
+template<typename T, unsigned int S> inline simd_t<T,S> &operator/=(simd_t<T,S> &a, simd_t<T,S> b) { a.x /= b.x; return a; }
 
 template<typename T, unsigned int S> inline simd_t<T,S> operator+(simd_t<T,S> a, simd_t<T,S> b) { return a.x + b.x; }
 template<typename T, unsigned int S> inline simd_t<T,S> operator-(simd_t<T,S> a, simd_t<T,S> b) { return a.x - b.x; }
