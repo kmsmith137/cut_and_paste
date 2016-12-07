4ecfa98609861e394e8ed3a9b9b0d347eca0df20
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-07 12:01:36 -0500

    add time.hpp

---
+++ time.hpp
@@ -0,0 +1,15 @@
+#include <sys/time.h>
+#include <stdexcept>
+
+inline double time_diff(const struct timeval &tv1, const struct timeval &tv2)
+{
+    return (tv2.tv_sec - tv1.tv_sec) + 1.0e-6 * (tv2.tv_usec - tv1.tv_usec);
+}
+
+inline struct timeval get_time()
+{
+    struct timeval ret;
+    if (gettimeofday(&ret, NULL) < 0)
+        throw std::runtime_error("gettimeofday() failed");
+    return ret;
+}
