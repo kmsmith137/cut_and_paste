2581e2296f130b02b6a0e28725012101ea8eb114
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-07 12:00:24 -0500

    add aligned_alloc.hpp

---
+++ aligned_alloc.hpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+
+template<typename T>
+inline T *aligned_alloc(size_t nelts, size_t nalign=128, bool zero=true)
+{
+    if (nelts == 0)
+        return NULL;
+
+    // align to 64-byte cache lines
+    void *p = NULL;
+    if (posix_memalign(&p, nalign, nelts * sizeof(T)) != 0)
+        throw std::runtime_error("couldn't allocate memory");
+
+    if (zero)
+	memset(p, 0, nelts * sizeof(T));
+
+    return reinterpret_cast<T *> (p);
+}
