481b766efbeaba882be4e0fd3d6d696e6a6bd64e
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-12-08 21:45:44 -0500

    simd randomizers

--- simd_kernels/simd_debug.hpp
+++ simd_kernels/simd_debug.hpp
@@ -1,10 +1,102 @@
+#include <vector>
+#include <random>
+#include <cassert>
 #include <iostream>
+#include <type_traits>
+
 #include "simd_t.hpp"
 #include "simd_ntuple.hpp"
 #include "simd_trimatrix.hpp"
 
 
 // -------------------------------------------------------------------------------------------------
+//
+// vectorize(): unpacks a simd type to a std::vector
+
+
+template<typename T, unsigned int S>
+inline std::vector<T> vectorize(simd_t<T,S> x)
+{
+    std::vector<T> ret(S);
+    x.storeu(&ret[0]);
+    return ret;
+}
+
+
+template<typename T, unsigned int S, unsigned int N>
+inline std::vector<T> vectorize(const simd_ntuple<T,S,N> &v)
+{
+    std::vector<T> ret(N*S);
+    v.storeu(&ret[0]);
+    return ret;
+}
+
+
+template<typename T, unsigned int S, unsigned int N>
+inline std::vector<T> vectorize(const simd_trimatrix<T,S,N> &m)
+{
+    std::vector<T> ret((N*(N-1)*S)/2);
+    m.storeu(&ret[0]);
+    return ret;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+//
+// Randomizers.
+//
+// Reminder: an RNG is initialized with
+//
+//   std::random_device rd;
+//   std::mt19937 rng(rd());
+
+
+
+// The helper function _uniform_randvec() has syntax
+//    std::vector<T> _uniform_randvec(std::mt19937 &rng, unsigned int n, T lo, T hi);
+// but is implemented differently for integral and floating-point types.
+
+// Integral case: generate random number in range [lo,hi].  Note that endpoints are included in range.
+template<typename T, typename std::enable_if<std::is_integral<T>::value,int>::type = 0>
+inline std::vector<T> _uniform_randvec(std::mt19937 &rng, unsigned int n, T lo, T hi)
+{
+    std::vector<T> ret(n);
+    for (unsigned int i = 0; i < n; i++)
+	ret[i] = std::uniform_int_distribution<>(lo,hi)(rng);
+    return ret;
+}
+
+// Floating-point case
+template<typename T, typename std::enable_if<std::is_floating_point<T>::value,int>::type = 0>
+inline std::vector<T> _uniform_randvec(std::mt19937 &rng, unsigned int n, T lo, T hi)
+{
+    std::vector<T> ret(n);
+    for (unsigned int i = 0; i < n; i++)
+	ret[i] = lo + (hi-lo) * std::uniform_real_distribution<>()(rng);
+    return ret;
+}
+
+
+template<typename T, unsigned int S>
+inline simd_t<T,S> uniform_random_simd_t(std::mt19937 &rng, T lo, T hi)
+{
+    std::vector<T> v = _uniform_randvec<T> (rng, S, lo, hi);
+    return simd_t<T,S>::loadu(&v[0]);
+}
+
+template<typename T, unsigned int S, unsigned int N>
+inline simd_ntuple<T,S,N> uniform_random_simd_ntuple(std::mt19937 &rng, T lo, T hi)
+{
+    std::vector<T> v = _uniform_randvec<T> (rng, N*S, lo, hi);
+    simd_ntuple<T,S,N> ret;
+    ret.loadu(&v[0]);
+    return ret;
+}
+
+
+// -------------------------------------------------------------------------------------------------
+//
+// Print routines
 
 
 template<class T, unsigned int S, unsigned int M>
@@ -39,6 +131,8 @@ std::ostream &operator<<(std::ostream &os, simd_t<T,S> x)
 
 
 // -------------------------------------------------------------------------------------------------
+//
+// Print routines, part 2
 
 
 template<typename T, unsigned int S, unsigned int N>
