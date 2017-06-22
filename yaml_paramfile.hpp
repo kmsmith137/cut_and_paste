d6903552ad09ebfba51fe56d4964187ee05915d0
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-06-22 07:43:14 -0400

    yaml_paramfile: verbosity, read_scalar default value

--- yaml_paramfile.hpp
+++ yaml_paramfile.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <sstream>
 #include <stdexcept>
 #include <unordered_set>
 
@@ -12,21 +13,48 @@
 struct yaml_paramfile {
     const std::string filename;
     YAML::Node yaml;
+    int verbosity;
 
     std::unordered_set<std::string> all_keys;
     mutable std::unordered_set<std::string> requested_keys;
 
-    yaml_paramfile(const std::string &filename);
+    // The 'verbosity' constructor argument has the following meaning:
+    //   0 = quiet
+    //   1 = announce default values for all unspecified parameters
+    //   2 = announce all parameters
+    yaml_paramfile(const std::string &filename, int verbosity=0);
 
     bool has_param(const std::string &k) const;
     void check_for_unused_params(bool fatal=true) const;
 
-    // By default, throws an exception
-    virtual void _die(const std::string &txt) const;
-
-    // For debugging
-    template<typename T> static inline std::string type_name();
+    virtual void _die(const std::string &txt) const;    // by default, throws an exception
+    virtual void _print(const std::string &txt) const;  // by default, prints to cout
     
+    // For debugging and message-printing
+    template<typename T> static inline std::string type_name();
+    template<typename T> static inline std::string stringify(const T &x) { std::stringstream ss; ss << x; return ss.str(); }
+
+    // _read_scalar(): assumes key exists
+    template<typename T>
+    T _read_scalar(const std::string &k) const
+    {
+	T ret;
+
+	try {
+	    ret = yaml[k].as<T> ();
+	}
+	catch (...) {
+	    _die(std::string("expected '") + k + std::string("' to have type ") + type_name<T>());
+	    return 0;  // compiler pacifier
+	}
+
+	if (verbosity >= 2)
+	    _print(k + " = " + stringify(ret) + "\n");
+
+	return ret;
+    }
+
+    // "Vanilla" version of read_scalar()
     template<typename T>
     T read_scalar(const std::string &k) const
     {
@@ -35,13 +63,22 @@ struct yaml_paramfile {
 	if (!has_param(k))
 	    _die("parameter '" + k + "' not found");
 
-	try {
-	    return yaml[k].as<T> ();
-	}
-	catch (...) { }
+	return _read_scalar<T> (k);
+    }
 
-	_die(std::string("expected '") + k + std::string("' to have type ") + type_name<T>());
-	return 0;   // compiler pacifier
+    // This version of read_scalar() has a default value, which is returned if the key is not found.
+    template<typename T>
+    T read_scalar(const std::string &k, T default_val) const
+    {
+	requested_keys.insert(k);
+
+	if (!has_param(k)) {
+	    if (verbosity >= 1)
+		_print("parameter '" + k + "' not found, using default value " + stringify(default_val) + "\n");
+	    return default_val;
+	}
+
+	return _read_scalar<T> (k);
     }
 
     // Automatically converts a scalar to length-1 vector.
@@ -70,6 +107,7 @@ struct yaml_paramfile {
 
 
 template<> inline std::string yaml_paramfile::type_name<int> () { return "int"; }
+template<> inline std::string yaml_paramfile::type_name<float> () { return "float"; }
 template<> inline std::string yaml_paramfile::type_name<double> () { return "double"; }
 template<> inline std::string yaml_paramfile::type_name<std::string> () { return "string"; }
 
